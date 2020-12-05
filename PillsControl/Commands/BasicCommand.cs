fff9eb6ee429043dfdae5bca0d9a931ade4d737c
livvy7878 <live78787878@gmail.com>
2020-12-05 18:55:59 +0200

    Created command base class

---
+++ PillsControl/Commands/BasicCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PillsControl.Commands
+{
+	class BasicCommand : ICommand
+	{
+		private Func<object, bool> _canExecute;
+		private Action<object> _execute;
+
+		public bool CanExecute(object? parameter)
+		{
+			return _canExecute == null || _canExecute(parameter);
+		}
+
+		public void Execute(object? parameter)
+		{
+			_execute(parameter);
+		}
+
+		public event EventHandler? CanExecuteChanged
+		{
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
+		}
+	}
+}
